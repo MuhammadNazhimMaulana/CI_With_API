4fc33666e5e4bbf94a4db5fb20cb6eac75964514
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2021-10-07 21:04:33 +0700

    Finishing input and preparing update

--- latihan-CI/app/Controllers/Admin/Data_A.php
+++ latihan-CI/app/Controllers/Admin/Data_A.php
@@ -157,13 +157,77 @@ class Data_A extends BaseController
             
             $resp = curl_exec($curl);
 
+            $respons = json_decode($resp, true);
+
             $respon = [
                 'title' => 'Hasil Input',
-                'datas' => $resp
+                'datas' => $respons
             ];
 
+            // Akan redirect
+            return view('View_Data/result_data', $respon);
         }
 
         return view('View_Data/insert_data', $data_insert);
+    }
+
+    public function update()
+    {
+        $id = $this->request->uri->getSegment(4);
+
+        // Get cURL resource
+        $curl = curl_init();
+        // Set some options - we are passing in a useragent too here
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://jsonplaceholder.typicode.com/posts/'.$id,
+            CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+        ));
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+
+        // Json Decode
+        $decode = json_decode($resp);
+
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        $data_update = [
+            'title' => 'Update Data Using Curl',
+            'datas' => $decode
+        ];
+
+        if($this->request->getPost())
+        {
+            $url = "https://jsonplaceholder.typicode.com/posts";
+    
+            $data_array = array(    
+                'name' => $_POST['name'],
+                'job' => $_POST['job']
+            );
+            
+            $data = http_build_query($data_array);
+            
+            $ch = curl_init();
+            
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            
+            $resp = curl_exec($ch);
+
+            $respons = json_decode($resp, true);
+
+            $respon = [
+                'title' => 'Hasil Input',
+                'datas' => $respons
+            ];
+
+            // Akan redirect
+            return view('View_Data/result_data', $respon);
+        }
+
+        return view('View_Data/update_data', $data_update);
     }
 }
